5d4a4e037e0c0181d6648536fa4f1fa2cbf65ffd
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-03-30 13:16:52 +0600

    dekstop

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.gallery__slider').slick({
+        arrows: true,
+        dots: false,
+        adaptiveHeight: true,
+        slidesToShow: 3,
+        speed: 1000,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        waitForAnimate: false,
+        // centerMode: true,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                arrows: false
+              }
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                  slidesToShow: 1
+                }
+              },
+          ]
+    });
+});
--- tasks/js.js
+++ tasks/js.js
@@ -2,6 +2,7 @@ const { src, dest } = require("gulp");
 
 // Плагины
 const plumber = require("gulp-plumber");
+const concat = require("gulp-concat");
 const notify = require("gulp-notify");
 const babel = require("gulp-babel");
 // const uglify = require("gulp-uglify");
@@ -14,6 +15,7 @@ const app = require("../config/app.js");
 // обработка JavaScript
 const js = () => {
     return src(path.js.src, { sourcemaps:app.isDev })
+    .pipe(concat("main.js"))
     .pipe(plumber({
         errorHandler: notify.onError(error => ({
             title: "JavaScript",
@@ -21,7 +23,7 @@ const js = () => {
         }))
     }))
     .pipe(babel())
-    .pipe(webpack(app.webpack))
+    // .pipe(webpack(app.webpack))
 //    .pipe(uglify())
     .pipe(dest(path.js.dest, { sourcemaps: app.isDev }))
 };
